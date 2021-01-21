e2bd3a801ae27f877ae602233322f0c8e093fdcc
Alexchernoshtan <48185881+Alexchernoshtan@users.noreply.github.com>
2021-01-21 14:24:32 +0300

    Add files via upload

---
+++ Library/ViewModels/BookListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class BookListViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public SelectList Categories { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Library/ViewModels/CreateBookViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Library.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.ViewModels
+{
+    public class CreateBookViewModel
+    {
+        public string Name { get; set; }
+
+        public string Desc { get; set; }
+        public bool IsFavourite { get; set; }
+        public bool IsNew { get; set; }
+        public bool IsRecomended { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public IFormFile Img { get; set; }
+        public IFormFile File { get; set; }
+
+    }
+}
